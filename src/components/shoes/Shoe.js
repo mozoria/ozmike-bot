9af6435b607541fb9aa329274a9effde3b9ff52a
mozoria <m.ozoria@yahoo.com>
2020-01-29 09:05:42 -0500

    Altered the boolean statement
    Changed the boolean statement to display edit/delete button
    removed owner_id in order to make the statement true

--- src/components/shoes/Shoe.js
+++ src/components/shoes/Shoe.js
@@ -43,8 +43,6 @@ class Shoe extends Component {
     return (
       <div>
         <h2>{this.state.shoe.name}</h2>
-        <h2>{this.props.user._id}</h2>
-        <h2>{this.state.shoe.owner._id}</h2>
         {this.props.user && (this.props.user._id === this.state.shoe.owner) &&
             (
               <div>
